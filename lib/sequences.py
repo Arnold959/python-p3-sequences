1a3cf60e488643340aee91094b9319a3193023b1
arnold <musaliokello@gmail.com>
2024-03-14 17:25:30 +0300

    changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
+def print_fibonacci(length): 
+    if length <= 0:
+        print("[]")
+        return
+    fib =[0,1]
+    if length == 1:
+        print([0])
+        return
+    for  i in range(2,length):
+        fib.append(fib[i-1]+fib[i-2])
+        print(fib)
     pass
